0edca8fd84759272299581fdbf98a0a91b1be961
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2026-01-16 08:04:08 -0300

    build product model/repository

---
+++ server/src/main/java/com/learn/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.learn.repository;
+
+import com.learn.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
